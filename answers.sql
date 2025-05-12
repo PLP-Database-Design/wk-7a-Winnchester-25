6433ef13ca204fa319e39b38a16b1cea2f5c18a9
Winnchester-25 <winnchester17@gmail.com>
2025-05-12 16:39:49 +0100

    Wk 7 Assignment

--- answers.sql
+++ answers.sql
@@ -13,7 +13,7 @@ CREATE TABLE orders (
 
 CREATE TABLE products (
   productID INT AUTO_INCREMENT PRIMARY KEY,
-  productName VARCHAR(100),
+  productName VARCHAR(100)
   
 );
 
@@ -23,19 +23,22 @@ CREATE TABLE order_items (
  productID INT,
  FOREIGN KEY (orderID) REFERENCES orders(orderID),
  FOREIGN KEY (productID) REFERENCES products(productID)
-);
+); 
+
+-- By creating individual tables, i ensure that each row has a single single product per hour
 
 -- QUESTION 2
 
+
 CREATE TABLE customers (
     customerID INT AUTO_INCREMENT PRIMARY KEY,
-    customerName VARCHAR(100),
+    customerName VARCHAR(100)
     
 );
 
 CREATE TABLE products (
     productID INT AUTO_INCREMENT PRIMARY KEY,
-    productName VARCHAR(100),
+    productName VARCHAR(100)
 
 );
 
@@ -52,6 +55,8 @@ CREATE TABLE order_details (
     orderID INT,
     productID INT,
     quantity INT,
-    FOREIGN Key(orderID) REFERENCES orders (orderID),
-    FOREIGN Key(productID) REFERENCES products (productID)
-)
+    FOREIGN Key(orderID) REFERENCES orders(orderID),
+    FOREIGN Key(productID) REFERENCES products(productID)
+);
+
+-- By creating individual tables for the customerName and orderID, i have ensured that the 1NF has been maintained and that both columns are not dependent on each other. This ensures that the quantity of producrs ordered will depend on the primary keys of the orderID and customerID
